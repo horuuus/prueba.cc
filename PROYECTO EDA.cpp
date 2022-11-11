0f3f3bb2c85d9f2295ee643d6b85f3a829100e15
Horusky <70548647+Horusky@users.noreply.github.com>
2022-11-11 15:02:43 -0500

    structs
    Primera parte (creación de structs)
    Soy Alonso Salazar

---
+++ PROYECTO EDA.cpp
@@ -0,0 +1,55 @@
+#include <iostream>
+#include <cstdlib>
+#include <cstring>
+#include <windows.h>
+
+
+using namespace std;
+
+struct paciente{
+	char nombre[20];
+	struct paciente *sgte;
+};
+
+struct medico{
+	char nombre[20];
+	char especialidad[100];
+	struct medico *sgte;
+	struct paciente *proximo;
+	struct carga *Hist_Clin_Paciente;
+	struct  disponibilidadcamas *habita;
+	struct farmacia * lote;
+};
+
+struct Hist_Clin_Paciente{
+	char enfemerdad[50];
+	char estado[50];
+	char tratamientos[50];
+	struct Hist_Clin_Paciente *sgte;
+};
+
+struct disponibilidadcamas{
+    char cantidad[50];
+    char ubicacion[50];
+    char nombre_cama[50];
+    struct disponibilidadcamas *sgte;
+};
+
+struct farmacia{
+	char cantidades[50];
+	char codigo_medicamento[50];
+	char tipo_medicamento[50];
+	char estado_medicamento[50];
+	struct farmacia *sgte;
+};
+
+typedef struct paciente *Paciente;
+typedef struct medico *Medico;
+typedef struct Hist_Clin_Paciente *Historial;
+typedef struct disponibilidadcamas *Camas;
+typedef struct farmacia *Medicamentos;
+
+int main(){
+	
+	return 0;
+}
